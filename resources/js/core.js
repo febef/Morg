5f7928e518981a86e357aa11f375cc67b1f1f2f7
Facundo E. Beltramo <fexbef@gmail.com>
2015-03-14 03:45:15 -0300

    vsupts!

--- resources/js/core.js
+++ resources/js/core.js
@@ -10,8 +10,8 @@ var core = {
       $.get('/m/player', function(data) {
          $('#player').html(data);
       });
-      $.get('/m/downloadmanager', function(data) {
-         $('#downloadmanager').html(data);
+      $.get('/m/netmanager', function(data) {
+         $('#netmanager').html(data);
       });
    },
    hide : function(selector) {
@@ -21,12 +21,12 @@ var core = {
       if (!core.ischPanel)
          core.ischPanel = true;
          $.get('/p/'+panel, function(data) {
-            if (data=='ok') {
-               $("#labelpanel").html("loading...");
-               $.get('/p', function(data) {
-                  $("#panel").html(data);
-                  $("#labelpanel").html(panel);
-               });
+            if (data !== '!') {
+             //  $("#labelpanel").html("loading...");
+             //  $.get('/p', function(data) {
+               $("#panel").html(data);
+               $("#labelpanel").html(panel);
+             //  })
             core.ischPanel = false;
             }
          });
